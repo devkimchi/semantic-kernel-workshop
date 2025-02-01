45cc9c9c67e97303c9248838ad6df9cf4e2c18bf
Justin Yoo <justin.yoojh@gmail.com>
2025-02-01 12:23:43 +0900

    Add doc for step 02

---
+++ save-points/step-02/complete/Workshop.ConsoleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ClientModel;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+using OpenAI;
+
+using Workshop.ConsoleApp;
+
+var config = new ConfigurationBuilder()
+                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                 .AddUserSecrets<Program>()
+                 .Build();
+
+var client = new OpenAIClient(
+    credential: new ApiKeyCredential(config["GitHub:Models:AccessToken"]!),
+    options: new OpenAIClientOptions { Endpoint = new Uri(config["GitHub:Models:Endpoint"]!) });
+
+var kernel = Kernel.CreateBuilder()
+                   .AddOpenAIChatCompletion(
+                        modelId: config["GitHub:Models:ModelId"]!,
+                        openAIClient: client)
+                   .Build();
+
+// Uncomment each line to run a plugin
+await PluginAction.InvokeInlinePromptAsync(kernel);
+// await PluginAction.InvokeImportedPromptAsync(kernel);
+// await PluginAction.InvokeTrainBookingPluginAsync(kernel);
+// await PluginAction.InvokeWeatherPluginAsync(kernel);
